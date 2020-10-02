fc2ba20a788b2b4d4fc806f087b322cbb39a5d64
cherkasovkv <cherkasovkv1992@gmail.com>
2020-10-02 20:09:24 +0300

    image save fix

--- app/src/main/java/com/furianrt/mydiary/domain/save/SaveImagesUseCase.kt
+++ app/src/main/java/com/furianrt/mydiary/domain/save/SaveImagesUseCase.kt
@@ -17,7 +17,6 @@ import com.furianrt.mydiary.model.gateway.image.ImageGateway
 import com.furianrt.mydiary.domain.UriToRealPathUseCase
 import com.furianrt.mydiary.utils.generateUniqueId
 import io.reactivex.rxjava3.core.Completable
-import io.reactivex.rxjava3.core.Flowable
 import javax.inject.Inject
 
 class SaveImagesUseCase @Inject constructor(
@@ -27,18 +26,14 @@ class SaveImagesUseCase @Inject constructor(
 ) {
 
     operator fun invoke(noteId: String, imageUris: List<String>): Completable =
-            Flowable.fromIterable(imageUris)
-                    .flatMapSingle { uri ->
-                        uriToRealPathUseCase(uri)
-                                .map { MyImage(noteId + "_" + generateUniqueId(), it, noteId) }
-                                .flatMap { imageGateway.saveImageToStorage(it) }
-                                .flatMapCompletable { imageGateway.insertImage(it) }
-                                .andThen(Completable.fromAction { deviceGateway.clearUriTempFiles() }
-                                        .toSingleDefault(true))
-                                .onErrorReturn { false }
-                    }
-                    .collectInto(mutableListOf<Boolean>()) { l, i -> l.add(i) }
-                    .flatMapCompletable { Completable.fromAction { deviceGateway.clearUriTempFiles() } }
+            Completable.concat(imageUris.map { saveImage(noteId, it) })
+
+    private fun saveImage(noteId: String, imageUri: String): Completable =
+            uriToRealPathUseCase(imageUri)
+                    .map { MyImage(noteId + "_" + generateUniqueId(), it, noteId) }
+                    .flatMap { imageGateway.saveImageToStorage(it) }
+                    .flatMapCompletable { imageGateway.insertImage(it) }
+                    .andThen(Completable.fromAction { deviceGateway.clearUriTempFiles() })
 
     operator fun invoke(noteId: String, bitmap: Bitmap): Completable =
             imageGateway.saveBitmapToStorage(bitmap, MyImage(noteId + "_" + generateUniqueId(), "", noteId))
