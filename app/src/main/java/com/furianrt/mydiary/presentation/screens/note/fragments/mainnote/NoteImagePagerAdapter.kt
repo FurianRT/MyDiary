9618813fb873c67cee1a815949fb54a19a92e85b
cherkasovkv <cherkasovkv1992@gmail.com>
2020-10-03 01:35:24 +0300

    removed panoramic photos 2

--- app/src/main/java/com/furianrt/mydiary/presentation/screens/note/fragments/mainnote/NoteImagePagerAdapter.kt
+++ app/src/main/java/com/furianrt/mydiary/presentation/screens/note/fragments/mainnote/NoteImagePagerAdapter.kt
@@ -10,17 +10,23 @@
 
 package com.furianrt.mydiary.presentation.screens.note.fragments.mainnote
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.target.CustomViewTarget
+import com.bumptech.glide.request.transition.Transition
 import com.bumptech.glide.signature.ObjectKey
 import com.furianrt.mydiary.R
 import com.furianrt.mydiary.model.entity.MyImage
 import com.furianrt.mydiary.presentation.general.GlideApp
+import com.furianrt.mydiary.utils.animateAlpha
 import kotlinx.android.synthetic.main.note_image_pager_item.view.*
 
 class NoteImagePagerAdapter(
@@ -49,12 +55,25 @@ class NoteImagePagerAdapter(
     inner class NoteImageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         fun bind(image: MyImage) {
             itemView.setOnClickListener { listener?.onImageClick(image) }
+            itemView.image_note.alpha = 0f
             GlideApp.with(itemView)
+                    .asBitmap()
                     .load(Uri.parse(image.path))
-                    .signature(ObjectKey(image.editedTime.toString() + image.name))
                     .centerCrop()
+                    .signature(ObjectKey(image.editedTime.toString() + image.name))
                     .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
-                    .into(itemView.image_note)
+                    .into(object : CustomViewTarget<ImageView, Bitmap>(itemView.image_note) {
+                        override fun onResourceCleared(placeholder: Drawable?) = Unit
+                        override fun onLoadFailed(errorDrawable: Drawable?) {
+                            listener?.onImageLoadFailed()
+                        }
+
+                        override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                            listener?.onImageLoaded()
+                            itemView.image_note.setImageBitmap(resource)
+                            itemView.image_note.animateAlpha(0f, 1f)
+                        }
+                    })
         }
     }
 
