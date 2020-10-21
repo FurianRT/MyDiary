a3a10f2f97b5f1019bc8a13ebb1b5547142ba6a1
cherkasovkv <cherkasovkv1992@gmail.com>
2020-10-21 04:45:48 +0300

    billing template

--- app/src/main/java/com/furianrt/mydiary/model/source/billing/inapp/InAppBillingSource.kt
+++ app/src/main/java/com/furianrt/mydiary/model/source/billing/inapp/InAppBillingSource.kt
@@ -1,4 +1,8 @@
 package com.furianrt.mydiary.model.source.billing.inapp
 
+import com.furianrt.mydiary.model.source.billing.entity.ConnectionState
+import io.reactivex.rxjava3.core.Observable
+
 interface InAppBillingSource {
+    fun connectToBillingServices(): Observable<ConnectionState>
 }
--- app/src/main/java/com/furianrt/mydiary/model/source/billing/inapp/InAppBillingSourceImp.kt
+++ app/src/main/java/com/furianrt/mydiary/model/source/billing/inapp/InAppBillingSourceImp.kt
@@ -1,7 +1,49 @@
 package com.furianrt.mydiary.model.source.billing.inapp
 
+import android.content.Context
+import com.android.billingclient.api.BillingClient
+import com.android.billingclient.api.BillingClient.BillingResponseCode
+import com.android.billingclient.api.BillingClientStateListener
+import com.android.billingclient.api.BillingResult
+import com.furianrt.mydiary.di.application.modules.app.AppContext
+import com.furianrt.mydiary.model.source.billing.entity.ConnectionState
+import io.reactivex.rxjava3.core.Observable
 import javax.inject.Inject
 
-class InAppBillingSourceImp @Inject constructor(): InAppBillingSource {
+class InAppBillingSourceImp @Inject constructor(@AppContext context: Context): InAppBillingSource {
 
+    companion object {
+        private var sIsConnected = false
+    }
+
+    private val billingClient = BillingClient.newBuilder(context)
+            .setListener { billingResult, purchases ->
+                if (billingResult.responseCode == BillingResponseCode.OK && purchases != null) {
+
+                } else if (billingResult.responseCode == BillingResponseCode.USER_CANCELED) {
+
+                } else {
+
+                }
+            }
+            .build()
+
+    override fun connectToBillingServices(): Observable<ConnectionState> = Observable.create { emitter ->
+        billingClient.startConnection(object : BillingClientStateListener {
+            override fun onBillingSetupFinished(result: BillingResult) {
+                if (result.responseCode == BillingResponseCode.OK) {
+                    sIsConnected = true
+                    emitter.onNext(ConnectionState.Connected)
+                } else {
+                    sIsConnected = false
+                    emitter.onNext(ConnectionState.Error.Unknown())
+                }
+            }
+
+            override fun onBillingServiceDisconnected() {
+                sIsConnected = false
+                emitter.onNext(ConnectionState.Disconnected)
+            }
+        })
+    }
 }
